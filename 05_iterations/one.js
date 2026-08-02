10a89fd6003ea4377246e91762e884cc2abc6caf
Ansh Singh <singhanshak57@gmail.com>
2026-08-02 15:19:47 +0530

    JS

---
+++ 05_iterations/one.js
@@ -0,0 +1,28 @@
+// for loop
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if(element==5){
+      //  console.log("5 is the best number .")
+    }
+  //  console.log(element);
+}
+
+for (let i = 0; i <= 10; i++) {
+   // console.log(`Outer loop value: ${i}`);
+    for (let j = 0; j <= 10; j++) {
+     //   console.log(`Inner loop value: ${j} and inner loop: ${i}`);
+    }
+}
+
+
+let myArr = ["flash" , "batman" , "superman"]
+for (let i = 0; i < myArr.length; i++) {
+    const element = myArr[i];
+    // console.log(element);
+}
+
+
+// break and continue 
+// already know
+
